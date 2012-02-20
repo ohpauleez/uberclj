478a46c03b34faadf79a6c36a14c52b227efed13
Paul deGrandis <paul.degrandis@gmail.com>
2012-02-20 14:56:58 -0800

    lein run - now runs the uber_project.clj build step

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject uberclj "1.4.0-beta1-20120212"
   :description "Builds an uberjar for Clojure + Clojure-contrib"
   :dependencies [[org.clojure/clojure "1.4.0-beta1"]
-                 [clj-http "0.3.2"]])
+                 [clj-http "0.3.2"]]
+  :main uberclj.core/build-project)
 
--- test/uberclj/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns uberclj.test.core
-  (:use [uberclj.core])
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
